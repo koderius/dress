0e60284cc724fddb324111288ea18fe1c750dbbc
Moshe Estroti <mestroti@gmail.com>
2020-12-17 12:15:28 +0200

    with keys

---
+++ functions/src/keys.ts
@@ -0,0 +1,9 @@
+// The business account details:
+export const PaypalClient = 'AdIt6svcRhf9dt4Ewh4iW3mScy-CJlaV1iKtpFSfQy8pQLEphiMlf5QeoStNFOaxU-0H49eyRSyshwy7';
+export const PaypalSecret = 'EIU8S6tZ6ZIBERQ-qgj8wPqVSqmTZRIpEbeIYNz48GkggzYSze_jsXI-cLbjj5E8ZdjFVmWUwn5bJdCU';
+
+// The password for the no-reply mailbox
+export const NoReplyEmailPassword = 'mZQLBpLHeTpkz66DJcoH';
+
+/** Identifier for checking that the payment was committed from the app */
+export const AppIdentifier = 'DRESS@APP';
